2ecee60b9b3717b44ba852f88a52988cd044d691
Cao007 <1114685490@qq.com>
2025-04-10 00:23:20 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="content">
+		<text>{{title}}</text>
+	</view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const title = ref("ccc")
+</script>
+
+<style scoped lang="scss">
+	.content {
+		text {
+			color: pink;
+		}
+	}
+
+</style>
