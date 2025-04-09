7efa5ce94854555fcc79d00e01ab7cd7346a82ee
Cao007 <1114685490@qq.com>
2025-04-10 01:24:50 +0800

    feat: prettier代码格式化

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,19 +1,18 @@
 <template>
 	<view class="content">
-		<text>{{title}}</text>
+		<text>{{ title }}</text>
 	</view>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const title = ref("ccc")
+import { ref } from 'vue';
+const title = ref('ccc');
 </script>
 
 <style scoped lang="scss">
-	.content {
-		text {
-			color: pink;
-		}
+.content {
+	text {
+		color: pink;
 	}
-
+}
 </style>
--- pages/test1/test1.vue
+++ pages/test1/test1.vue
@@ -1,15 +1,15 @@
 <template>
 	<view class="content">
 		{{ title }}
+		<ul>
+			<li>123</li>
+		</ul>
 	</view>
 </template>
 
 <script setup>
-import {ref} from 'vue'
-const title = ref('hi')
-
+import { ref } from 'vue';
+const title = ref('hi');
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
